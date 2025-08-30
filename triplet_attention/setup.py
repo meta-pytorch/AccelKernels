d0b4436477b35feaa0b5da4fdae6cc1af3769269
Timothy Chou <timchou@meta.com>
2025-08-29 19:52:13 -0700

    Consolidate attn

--- triplet_attention/setup.py
+++
@@ -1,9 +0,0 @@
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-from setuptools import find_packages, setup
-
-setup(
-    name="triplet",
-    version="0.1.0",
-    packages=find_packages(include=["triplet"]),
-)
